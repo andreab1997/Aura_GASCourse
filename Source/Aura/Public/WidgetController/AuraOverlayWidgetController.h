b451110a8b20ea11fa67101b2aa0333e593a9398
andreab1997 <andreab1997@yahoo.it>
2025-08-24 13:27:28 +0200

    Now also adding The overlaywidgetcontroller class

---
+++ Source/Aura/Public/WidgetController/AuraOverlayWidgetController.h
@@ -0,0 +1,17 @@
+// Property of Andrea Barontini
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WidgetController/AuraWidgetController.h"
+#include "AuraOverlayWidgetController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraOverlayWidgetController : public UAuraWidgetController
+{
+	GENERATED_BODY()
+	
+};
